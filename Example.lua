d836c955e90301d8d149ae8ed5addae40bec35e4
j1scxxz-sketch <j1scxxz@gmail.com>
2026-07-22 22:28:31 +0000

    Update Example.lua

--- Example.lua
+++ Example.lua
@@ -1,7 +1,7 @@
 -- New example script written by wally
 -- You can suggest changes with a pull request or something
 
-local repo = 'https://raw.githubusercontent.com/violin-suzutsuki/LinoriaLib/main/'
+local repo = 'https://raw.githubusercontent.com/j1scxxz-sketch/kimika-haxx-/refs/heads/main/'
 
 local Library = loadstring(game:HttpGet(repo .. 'Library.lua'))()
 local ThemeManager = loadstring(game:HttpGet(repo .. 'addons/ThemeManager.lua'))()
